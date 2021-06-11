b6a99b3eee99d28aa0ef7591fe8bd6ebae3db20b
yosefAlsuhaibani <72322110+yosefAlsuhaibani@users.noreply.github.com>
2021-06-11 13:09:59 -0400

    Add server-side function fib; gets arg from client and returns result to client.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,17 +1,34 @@
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::str::FromStr;
 
 const ADDRESS: &str = "127.0.0.1:2000";
 
 fn main() -> std::io::Result<()> {
 
-    let input_address: Option<String> = std::env::args().nth(1); //Fix: .next_back() Always returned
-    let mut address: String = String::from(ADDRESS);             //A value, since if no args given,
-    if let Some(address_a) = input_address {                     //It returns Some(/target/../client)
-       address = address_a;
+    let input_address: Option<String> = std::env::args().nth(1); //An address
+    let func_arg: Option<String> = std::env::args().nth(2); //A u64 to be sent
+    let mut address: String = String::from(ADDRESS);
+    if let Some(address_a) = input_address {
+        address = address_a;
     }
 
-    let _s = std::net::TcpStream::connect(&address)?;
 
+    let mut s = std::net::TcpStream::connect(&address)?;
 
-
+    //If given a thrid arg, runs fib(arg) on server
+    if let Some(arg) = func_arg {
+       let x = u8::from_str(&arg).unwrap();
+       s.write(&[x])?;
+    }
+    loop {
+        let mut ans_buf = [0 as u8;8];
+        s.read(&mut ans_buf); //read Big endian byte stream; should be
+        let ans = u64::from_be_bytes(ans_buf);
+        if(ans != 0){
+            println!("The ans returned from the server is {}",ans);
+            break; //The server returned an answer; break from loop
+        }
+    }
     Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,11 +1,36 @@
+use std::io::prelude::*;
+use std::net::TcpStream; //I don't think I need this
+
+fn fib(n: u8) -> u64 {
+
+   if n == 0 {
+      return 1;
+   }
+   if n == 1 {
+      return 1;
+   }
+
+   fib(n - 1) + fib(n - 2)
+}
 
 
-fn handle(_s : std::net::TcpStream) {
+fn handle(mut s : std::net::TcpStream) {
+
+    let mut arg_buf = [0 as u8;1];
+    s.read(&mut arg_buf); //Read incoming arguments for fib
+
+    let mut x = 0;  //If no args, return fib(0)
+    if let Some(&y) = arg_buf.get(0)
+    {
+        x = y
+    }
 
     let f = inger::launch(|| {
-        println!("Launched ! ! !");
-        while true {}
-    }, u64::max_value());
+        let ans = fib(x);
+        println!("fib of {} is {}",x,ans);
+        s.write(& ans.to_be_bytes()).unwrap();
+        loop {}
+    }, 4000);
 
     if let Err (error) = f
     {
@@ -21,7 +46,7 @@ fn main() -> std::io::Result<()>{
     
    for s in listener.incoming()
    {
-       let s = s?;
+       let mut s = s?;
        std::thread::spawn(|| handle(s));
    }
    Ok(())

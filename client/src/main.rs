a750cfded0767681c67e409a0112bdf21ecd66d3
yosefAlsuhaibani <72322110+yosefAlsuhaibani@users.noreply.github.com>
2021-06-10 21:56:24 -0400

    Fixed indentation + Client bug when no arguments.

--- client/src/main.rs
+++ client/src/main.rs
@@ -1,7 +1,17 @@
-const ADDRESS: &str = "127.0.0.1:2000";
-fn main() -> std::io::Result<()>
-{
-   let _s = std::net::TcpStream::connect(ADDRESS)?;
 
-   Ok(())
+const ADDRESS: &str = "127.0.0.1:2000";
+
+fn main() -> std::io::Result<()> {
+
+    let input_address: Option<String> = std::env::args().nth(1); //Fix: .next_back() Always returned
+    let mut address: String = String::from(ADDRESS);             //A value, since if no args given,
+    if let Some(address_a) = input_address {                     //It returns Some(/target/../client)
+       address = address_a;
+    }
+
+    let _s = std::net::TcpStream::connect(&address)?;
+
+
+
+    Ok(())
 }
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,12 +1,15 @@
-fn h(_s : std::net::TcpStream)
-{
-   let f = inger::launch(|| {
-       println!("Launched ! ! !");
-       while true {}
-   }, u64::max_value());
+
+
+fn handle(_s : std::net::TcpStream) {
+
+    let f = inger::launch(|| {
+        println!("Launched ! ! !");
+        while true {}
+    }, u64::max_value());
+
     if let Err (error) = f
     {
-        eprintln!("ERROR: in launched function :{}",error)
+         eprintln!("ERROR: in launched function :{}",error)
     }
 
 }
@@ -19,7 +22,7 @@ fn main() -> std::io::Result<()>{
    for s in listener.incoming()
    {
        let s = s?;
-       std::thread::spawn(|| h(s));
+       std::thread::spawn(|| handle(s));
    }
    Ok(())
 }

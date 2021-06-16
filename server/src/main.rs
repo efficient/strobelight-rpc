dddc5cec2372fdb4e4d810456bceeaa18d8f530f
yosefAlsuhaibani <72322110+yosefAlsuhaibani@users.noreply.github.com>
2021-06-16 11:48:16 -0400

    Fixed server warrnings, had a bunch of unused 'Result<()>'s

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,23 +1,20 @@
+use std::fmt::Display;
 use std::io::prelude::*;
-use std::net::TcpStream; //I don't think I need this
+use std::net::TcpStream;
 
-
-
-
-
-fn handle(mut s : std::net::TcpStream) {
+fn handle(s : TcpStream) -> Result<(),Box<dyn Display>> {
 
     let mut s = std::io::BufReader::new(s);
     let mut data = String::default();
     //println!("The string before {}",data);
-    s.read_line(&mut data); //Read all incoming args; seperated by '\n'
+    s.read_line(&mut data).map_err(box_error)?; //Read all incoming args; seperated by '\n'
     data.pop();
     //println!("The string afert {}",data);
-    let func_id  = data.parse().unwrap();
+    let func_id  = data.parse().map_err(box_error)?;
     data.clear();
-    s.read_line(&mut data); //Read all incoming args; seperated by '\n'
+    s.read_line(&mut data).map_err(box_error)?; //Read all incoming args; seperated by '\n'
     data.pop();
-    let func_arg = data.parse().unwrap();
+    let func_arg = data.parse().map_err(box_error)?;
     let func = match func_id {
         0 => funs::fib,
         1 => funs::add2,
@@ -30,25 +27,34 @@ fn handle(mut s : std::net::TcpStream) {
         s.get_mut().write(format!("{}",ans).as_ref()).unwrap();
     }, 4000);
 
-    if let Err (error) = f
-    {
+    if let Err (error) = f {
          eprintln!("ERROR: in launched function :{}",error)
     }
     else if f.unwrap().is_continuation() {
         eprintln!("WARNING: FUNCTION TIMEDOUT!");
     }
-
+    Ok(())
 }
 
+fn box_error<'a,T: Display + 'a>(e: T) -> Box<dyn Display + 'a> {
 
-fn main() -> std::io::Result<()>{
+    Box::new(e)
+}
+
+fn handle_wrapper(s: TcpStream) {
+
+    if let Err(e) = handle(s) {
+        eprintln!("ERROR: {}",e);
+    }
+}
+
+fn main() -> std::io::Result<()> {
 	
    let listener =  std::net::TcpListener::bind("0.0.0.0:2000")?;
     
-   for s in listener.incoming()
-   {
-       let mut s = s?;
-       std::thread::spawn(|| handle(s));
+   for s in listener.incoming() {
+       let s = s?;
+       std::thread::spawn(|| handle_wrapper(s));
    }
    Ok(())
 }

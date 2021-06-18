5953c5fb98458b4316d3b30608ab584bab7bd6ac
yosefAlsuhaibani <72322110+yosefAlsuhaibani@users.noreply.github.com>
2021-06-17 21:38:39 -0400

    Instead of using the ".as_byte()" method, made writing to stream on the client side consistent with server

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,13 +1,13 @@
 use std::io::prelude::*;
+use serde::Serialize;
 use std::net::TcpStream;
 const ADDRESS: &str = "127.0.0.1:2000";
 
 
-
 pub fn rpc_client<'a, T: Iterator<Item = &'a str>>(mut args: T) -> std::io::Result<()> {
-    let input_address = args.next(); //An address
-    let func_id  = args.next().unwrap_or("0"); //A u8 to be sent
-    let func_arg = args.next().unwrap_or("0"); //A u8 to be sent
+    let input_address = args.next();           //An address
+    let func_id  = args.next().unwrap_or("0"); //A u64 to be sent
+    let func_arg = args.next().unwrap_or("0"); //A u64 to be sent
     let mut address = ADDRESS;
     if let Some(address_a) = input_address {
         address = address_a;
@@ -16,10 +16,10 @@ pub fn rpc_client<'a, T: Iterator<Item = &'a str>>(mut args: T) -> std::io::Resu
     let mut s = TcpStream::connect(&address)?;
 
     //write args; seperate by '\n'
-    s.write((&func_id).as_bytes())?;
-    s.write((&"\n").as_bytes())?;
-    s.write((&func_arg).as_bytes())?;
-    s.write((&"\n").as_bytes())?;
+    s.write(format!("{}",func_id).as_ref())?;
+    s.write(format!("{}","\n").as_ref())?;
+    s.write(format!("{}",func_arg).as_ref())?;
+    s.write(format!("{}","\n").as_ref())?;
     let mut ans_buf = String::default();
     s.read_to_string(&mut ans_buf)?;
     Ok(())

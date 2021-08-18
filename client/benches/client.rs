596ad596e71ef2ad4db8e361fef57513b94eb522
yosefAlsuhaibani <72322110+yosefAlsuhaibani@users.noreply.github.com>
2021-08-18 13:53:40 -0400

    Modifed get_args to notif. server to not run with Lib.inger

--- client/benches/client.rs
+++ client/benches/client.rs
@@ -4,13 +4,17 @@ use std::env;
 lazy_static! {
     static ref ADDRESS: String = env::var("ADDRESS").unwrap_or(String::from("localhost:2000"));
     static ref FUNC_ID: String = env::var("FUNC_ID").unwrap_or(String::from("1"));
-    static ref FUNC_ARG: String = env::var("FUNC_ARG").unwrap_or(String::from("1"));
+    static ref FUNC_ARG: String = env::var("FUNC_ARG").unwrap_or(String::from("20"));
     static ref FUNC_TIMEOUT: String = env::var("FUNC_TIMEOUT").unwrap_or(String::from("5000"));
 }
-fn get_env_args() -> impl Iterator<Item = &'static str> + Clone {
+
+fn get_env_args(is_inger: bool) -> impl Iterator<Item = &'static str> + Clone {
 
     let arg1: &str = &ADDRESS;
-    let arg2 = &FUNC_ID;
+    let mut arg2: &str = &FUNC_ID;
+    if !is_inger {
+        arg2 = "-1";
+    }
     let arg3 = &FUNC_ARG;
     let arg4 = &FUNC_TIMEOUT;
     vec![arg1,arg2,arg3,arg4].into_iter()
@@ -23,13 +27,13 @@ fn fib_local(var: &mut bencher::Bencher) {
 
 fn fib_remote(var: &mut bencher::Bencher) {
 
-    let args = get_env_args();
+    let args = get_env_args(false);
     var.iter(|| drop(client::rpc_client(args.clone())));
 }
 
 fn fib_remote_with_inger(var: &mut bencher::Bencher) {
 
-    let args = get_env_args();
+    let args = get_env_args(true);
     var.iter(|| drop(client::rpc_client(args.clone())));
 }
 

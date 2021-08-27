8a165c593ab71b7086536dd07a21f22d5d073cb0
yosefAlsuhaibani <72322110+yosefAlsuhaibani@users.noreply.github.com>
2021-08-27 16:05:08 -0400

    Changed client-timeout system to use TcpStream's set_read_timeout
    The problem with the old SystimeTime method was in the whileloop,
    control would be blocked when it tries to read anything coming through,
    thus havning no effect. Also, we needed to consider the case when the
    server timesout before the client; added err-handling to mimck
    client-timeout error at exit.

--- client/src/lib.rs
+++ client/src/lib.rs
@@ -1,7 +1,6 @@
 use funs::IngerRequest;
 use std::io::prelude::*;
 use std::net::TcpStream;
-use std::time::SystemTime;
 const ADDRESS: &str = "127.0.0.1:2000";
 
 pub fn rpc_client<'a, T: Iterator<Item = &'a str>>(mut args: T) -> std::io::Result<String> {
@@ -18,13 +17,20 @@ pub fn rpc_client<'a, T: Iterator<Item = &'a str>>(mut args: T) -> std::io::Resu
     }
 
     let mut s = TcpStream::connect(&address)?;
-    s.write(format!("{}\n",serde_json::to_string(&request).unwrap()).as_ref()).unwrap();
+    let timeout_set: u64 = request.func_timeout.parse().unwrap();
 
+    s.set_read_timeout(Some(std::time::Duration::from_micros(timeout_set))).unwrap();
+
+    s.write(format!("{}\n",serde_json::to_string(&request).unwrap()).as_ref()).unwrap();
     let mut ans_buf = String::default();
-    let client_timeout = SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos() + 10000;
-    while SystemTime::now().duration_since(SystemTime::UNIX_EPOCH).unwrap().as_nanos()  < client_timeout {
-        s.read_to_string(&mut ans_buf)?;
+    s.read_to_string(&mut ans_buf)?;
+
+    //Err handling; server times-out before client
+    if ans_buf == ""
+    {
+        Err(std::io::Error::new(std::io::ErrorKind::WouldBlock,"Resource temporarily unavailable"))?
     }
+
     Ok(ans_buf)
 }
 
